236e035c46dd7b3126a53decf4c3ebec8f0a7742
aymammeri <ay.mammeri@gmail.com>
2022-01-09 13:10:16 -0500

    Project change idea from whole-commerce to uFile
    Swap to  browser-template

--- app/models/user.js
+++ app/models/user.js
@@ -1,27 +1,25 @@
 const mongoose = require('mongoose')
 
 const userSchema = new mongoose.Schema({
-    email: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    hashedPassword: {
-        type: String,
-        required: true
-    },
-    token: String
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  hashedPassword: {
+    type: String,
+    required: true
+  },
+  token: String
 }, {
-    timestamp : true,
-    toJSON: {
-        virtuals: true,
-        // `user` is the returned Mongoose document
-        // named `user` because it will be a `User` object
-        transform: (_doc, user) => {
-          delete user.hashedPassword
-          return user
-        }
+  timestamps: true,
+  toObject: {
+    // remove `hashedPassword` field when we call `.toObject`
+    transform: (_doc, user) => {
+      delete user.hashedPassword
+      return user
     }
+  }
 })
 
 module.exports = mongoose.model('User', userSchema)
--- app/routes/user_routes.js
+++ app/routes/user_routes.js
@@ -1,103 +1,140 @@
 const express = require('express')
-const bcrypt = require('bcrypt')
+// jsonwebtoken docs: https://github.com/auth0/node-jsonwebtoken
 const crypto = require('crypto')
+// Passport docs: http://www.passportjs.org/docs/
 const passport = require('passport')
+// bcrypt docs: https://github.com/kelektiv/node.bcrypt.js
+const bcrypt = require('bcrypt')
+
+// see above for explanation of "salting", 10 rounds is recommended
+const bcryptSaltRounds = 10
+
+// pull in error types and the logic to handle them and set status codes
+const errors = require('../../lib/custom_errors')
+
+const BadParamsError = errors.BadParamsError
+const BadCredentialsError = errors.BadCredentialsError
+
 const User = require('../models/user')
 
-const router = express.Router()
+// passing this as a second argument to `router.<verb>` will make it
+// so that a token MUST be passed for that route to be available
+// it will also set `res.user`
 const requireToken = passport.authenticate('bearer', { session: false })
 
+// instantiate a router (mini app that only handles routes)
+const router = express.Router()
+
+// SIGN UP
+// POST /sign-up
 router.post('/sign-up', (req, res, next) => {
-    const credentials = req.body.credentials
-    // create hash with password
-    bcrypt.hash(credentials.password, 10)
-      .then(hash => {
-        // build user object with new hashed password
-        const user = {
-          email: credentials.email,
-          hashedPassword: hash
-        }
-        // save user to database
-        return User.create(user)
-      })
-    .then(user => res.status(201).json(user))
+  // start a promise chain, so that any errors will pass to `handle`
+  Promise.resolve(req.body.credentials)
+    // reject any requests where `credentials.password` is not present, or where
+    // the password is an empty string
+    .then(credentials => {
+      if (!credentials ||
+          !credentials.password ||
+          credentials.password !== credentials.password_confirmation) {
+        throw new BadParamsError()
+      }
+    })
+    // generate a hash from the provided password, returning a promise
+    .then(() => bcrypt.hash(req.body.credentials.password, bcryptSaltRounds))
+    .then(hash => {
+      // return necessary params to create a user
+      return {
+        email: req.body.credentials.email,
+        hashedPassword: hash
+      }
+    })
+    // create user with provided email and hashed password
+    .then(user => User.create(user))
+    // send the new user object back with status 201, but `hashedPassword`
+    // won't be send because of the `transform` in the User model
+    .then(user => res.status(201).json({ user: user.toObject() }))
+    // pass any errors along to the error handler
     .catch(next)
 })
 
+// SIGN IN
+// POST /sign-in
 router.post('/sign-in', (req, res, next) => {
-  // get password from request
-  const password = req.body.credentials.password
-  // declare user to access in the promise chain
+  const pw = req.body.credentials.password
   let user
-  // find the user by email
+
+  // find a user based on the email that was passed
   User.findOne({ email: req.body.credentials.email })
     .then(record => {
-      // if no user then throw error
+      // if we didn't find a user with that email, send 401
       if (!record) {
-        throw new Error('Document not found')
+        throw new BadCredentialsError()
       }
-      // if we find a record save to our user variable
+      // save the found user outside the promise chain
       user = record
-      // compare password and stored password
-      return bcrypt.compare(password, user.hashedPassword)
+      // `bcrypt.compare` will return true if the result of hashing `pw`
+      // is exactly equal to the hashed password stored in the DB
+      return bcrypt.compare(pw, user.hashedPassword)
     })
     .then(correctPassword => {
-      // if password is a match
+      // if the passwords matched
       if (correctPassword) {
-        // create a token
+        // the token will be a 16 byte random hex string
         const token = crypto.randomBytes(16).toString('hex')
-        // add token to user
         user.token = token
-        // save user
+        // save the token to the DB as a property on user
         return user.save()
-      // else then throw error
       } else {
-        throw new Error('Email or password is invalid')
+        // throw an error to trigger the error handler and end the promise chain
+        // this will send back 401 and a message about sending wrong parameters
+        throw new BadCredentialsError()
       }
     })
-    // respond with user and the token
     .then(user => {
-      res.status(201).json({ user })
+      // return status 201, the email, and the new token
+      res.status(201).json({ user: user.toObject() })
     })
     .catch(next)
 })
 
+// CHANGE password
+// PATCH /change-password
 router.patch('/change-password', requireToken, (req, res, next) => {
-  // declare user to access in the promise chain
   let user
-  // find user by their id
+  // `req.user` will be determined by decoding the token payload
   User.findById(req.user.id)
-    // if we find a record save to our user variable
-    .then(record => {
-      user = record
-      // compare old password and stored password
-      return bcrypt.compare(req.body.passwords.old, user.hashedPassword)
-    })
+    // save user outside the promise chain
+    .then(record => { user = record })
+    // check that the old password is correct
+    .then(() => bcrypt.compare(req.body.passwords.old, user.hashedPassword))
+    // `correctPassword` will be true if hashing the old password ends up the
+    // same as `user.hashedPassword`
     .then(correctPassword => {
-      // if the password is incorrect or there is no new password
-      if (!correctPassword || !req.body.passwords.new) {
-        throw new Error('A required parameter was omitted or invalid')
+      // throw an error if the new password is missing, an empty string,
+      // or the old password was wrong
+      if (!req.body.passwords.new || !correctPassword) {
+        throw new BadParamsError()
       }
-      // if password is correct then encrypt new password
-      return bcrypt.hash(req.body.passwords.new, 10)
     })
+    // hash the new password
+    .then(() => bcrypt.hash(req.body.passwords.new, bcryptSaltRounds))
     .then(hash => {
-      // add new has as hashed password for user
+      // set and save the new hashed password in the DB
       user.hashedPassword = hash
-      // save user
       return user.save()
     })
-    // respond with 204
-    .then((user) => res.status(200).json({ user }))
+    // respond with no content and status 200
+    .then(() => res.sendStatus(204))
+    // pass any errors along to the error handler
     .catch(next)
 })
 
 router.delete('/sign-out', requireToken, (req, res, next) => {
-  //create new token
-  req.user.token = crypto.randomBytes(16)
+  // create a new random token for the user, invalidating the current one
+  req.user.token = null
+  // save the token and respond with 204
   req.user.save()
-    // do not send new token back to client
-    .then(() => res.sendStatus(200))
+    .then(() => res.sendStatus(204))
     .catch(next)
 })
 
