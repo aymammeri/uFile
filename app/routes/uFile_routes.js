f99ceea4d310fe3543707ae1bfdd8de25e8ae8e4
aymammeri <ay.mammeri@gmail.com>
2022-01-15 17:48:57 -0500

    Create express routes file to handle uFile CRUDs
    Add CRUD routes for upload file, Index, get file, delete

--- app/routes/uFile_routes.js
+++ app/routes/uFile_routes.js
@@ -1,35 +1,59 @@
 const express = require('express')
+const passport = require('passport')
+const multer = require('multer')
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './uploads')
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname)
+  }
+})
+
+const upload = multer({storage: storage})
+
+const File = require('../models/file')
+
+const requireToken = passport.authenticate('bearer', { session: false })
 
 const router = express.Router()
 
-router.post('/upload-avatar', async (req, res) => {
-  try {
-    if (!req.files) {
-      res.send({
-        status: false,
-        message: 'No file uploaded'
-      })
-    } else {
-      // Use the name of the input field (i.e. "avatar") to retrieve the uploaded file
-      let avatar = req.files.avatar
+router.post('/upload', requireToken, upload.single('file'), (req, res, next) => {
+  console.log(req.file)
+  File.create({
+    name: req.file.originalname,
+    srcFile: req.file.path,
+    owner: req.user._id
+  })
+    .then(() => res.status(201).send('File uploaded successfully! '))
+    .catch(next)
+})
 
-      // Use the mv() method to place the file in upload directory (i.e. "uploads")
-      avatar.mv('./uploads/' + avatar.name)
+router.get('/files/:id', requireToken, (req, res, next) => {
+  File.findOne({ owner: req.user.id, _id: req.params.id })
+    .then(file => res.status(201).json(file))
+    .catch(next)
+})
 
-      // send response
-      res.send({
-        status: true,
-        message: 'File is uploaded',
-        data: {
-          name: avatar.name,
-          mimetype: avatar.mimetype,
-          size: avatar.size
-        }
-      })
-    }
-  } catch (err) {
-    res.status(500).send(err)
-  }
+router.get('/files', requireToken, (req, res, next) => {
+  File.find({ owner: req.user.id })
+    .then(file => res.status(201).json(file))
+    .catch(next)
+})
+
+router.patch('/files/:id', requireToken, (req, res, next) => {
+  File.findOne({ owner: req.user.id, _id: req.params.id })
+    .then()
+})
+
+router.delete('/files/delete', requireToken, (req, res, next) => {
+  File.findOne({ _id: req.body.fileID, owner: req.user._id })
+  // .then(file => console.log(file.srcFile))
+  // .then(file => fs.unlink(file.srcFile))
+  File.deleteOne({ _id: req.body.fileID, owner: req.user._id })
+    .then(file => res.status(201).json(file))
+    .catch(next)
 })
 
 module.exports = router
